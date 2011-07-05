0a5026a0ad2fcc3c1e5bac8ec5ff3b962708db22
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-07-05 11:10:49 +1000

    Created create_generator, and stubbed out template for it

---
+++ lib/generators/rails_jquery_selectors/create_generator.rb
@@ -0,0 +1,20 @@
+require 'rails/generators'
+
+module RailsJquerySelectors
+  class CreateGenerator < Rails::Generators::Base
+    desc 'Generate rails_jquery_selectors.js.coffee, containing selectors for the current state of your models'
+
+    def self.source_root
+      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    end
+
+    def generate
+      puts "generate"
+      Dir["#{Rails.root}/**/models/**/*.rb"].map { |file| require file }
+      ActiveRecord::Base.descendants.each do |model|
+        instance = model.new
+        template "rails_jquery_selectors", "app/assets/javascripts/rails_jquery_selectors"
+      end
+    end
+  end
+end
